88af25c33823cd80f240b9b0eb6669911a7ca5ce
nick <nick@elitehrv.com>
2021-03-06 04:11:43 +0200

    add Spren

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "spren",
+            name: "Spren",
             url: "https://api.codemagic.io/artifacts/bc09fb00-1b2a-40c4-b95c-0d97759a925d/4b4da198-76ee-4a57-8980-a9c1351260ed/Spren_iOS_5_artifacts.zip",
             checksum: "565d53091ac859121158dff4e830d9db98e9a1bda6f05a71038845806b16fc97"
         ),

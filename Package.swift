aeaa30c04d46d2221ae9fc3c1f3da5c49d1fba56
nick <nick@elitehrv.com>
2021-12-07 00:09:47 +0200

    Prepare 0.0.6 Release

--- Package.swift
+++ Package.swift
@@ -9,22 +9,15 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Spren",
             targets: ["Spren"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Spren",
-            url: "https://api.codemagic.io/artifacts/2c5fa87c-f435-422d-ab1f-63c2b5c445a4/bb932ae7-9f3c-41c1-b2fe-7d66fb3a6ce9/Spren_iOS_19_artifacts.zip",
-            checksum: "7b3c7d1e9de0324ec7d077b310f40a8257fa0f131706099aed8d588f99d821e2"
+            path: "Framework/Spren.xcframework.zip"
         ),
     ]
 )

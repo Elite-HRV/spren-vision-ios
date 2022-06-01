01ceeaa72efdf2255c2048c90d29c1219079f58e
nick <nick@elitehrv.com>
2022-06-02 00:38:59 +0300

    revert changes

--- flutter/example/lib/route/camera/camera.dart
+++ flutter/example/lib/route/camera/camera.dart
@@ -21,8 +21,8 @@ class CameraState extends State<RouteCamera> {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width - 100;
-    double height = MediaQuery.of(context).size.height - 100;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     Wakelock.enable();
 

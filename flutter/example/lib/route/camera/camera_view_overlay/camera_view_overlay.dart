2a42ddf4479a3288c3ac00103d9b55dde350eb3d
nick <nick@elitehrv.com>
2022-06-28 01:09:01 +0300

    Android: deprecate dropComplexity, remove toggle flash, iOS add toggle back

--- flutter/example/lib/route/camera/camera_view_overlay/camera_view_overlay.dart
+++ flutter/example/lib/route/camera/camera_view_overlay/camera_view_overlay.dart
@@ -1,5 +1,6 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:spren_flutter/spren_flutter.dart';
 import 'package:spren_flutter/spren_model.dart';
@@ -250,6 +251,10 @@ class CameraViewOverlay extends HookWidget {
                               CameraProgress(progress: progress.value),
                             ],
                           ),
+                          if (flash.value == 1 && defaultTargetPlatform == TargetPlatform.iOS)
+                            FlashEnableButton(notifyParent: changeTorchMode),
+                          if (flash.value == 0 && defaultTargetPlatform == TargetPlatform.iOS)
+                            FlashDisableButton(notifyParent: changeTorchMode),
                         ],
                       ),
                     )))),

c918c2c8fffe742a437612dd0669da31700bb94e
Keith Carolus <16885297+cosmicvarion@users.noreply.github.com>
2022-10-18 11:35:17 -0400

    fix RR card

--- Sources/SprenUI/UI/Organisms/DemographicRRCard.swift
+++ Sources/SprenUI/UI/Organisms/DemographicRRCard.swift
@@ -16,13 +16,18 @@ struct DemographicRRCard: View {
         VStack {
             VStack {
                 HStack {
-                    ResultCircle(value: "\(String(format: "%.0f", breathingRate))", unit: "rpm", color: breathingRate >= RespiratoryConstants.rangeMin && breathingRate <= RespiratoryConstants.rangeMax ? Color.green : Color("Yellow", bundle: .module))
-                    Text("Respiratory Rate").font(.sprenProgress).sprenUIPadding([.leading])
+                    ResultCircle(value: "\(String(format: "%.0f", breathingRate))",
+                                 unit: "rpm",
+                                 color: RRConstants.getColor(value: breathingRate))
+                    Text("Respiratory Rate")
+                        .font(.sprenProgress)
+                        .sprenUIPadding([.leading])
                     Spacer()
                 }.sprenUIPadding([.bottom])
                 
                 HStack {
-                    Text("Your respiratory rate of \(String(format: "%.0f", breathingRate)) rpm is normal for healthy adults.").font(.sprenLabel)
+                    Text("Your respiratory rate of \(String(format: "%.0f", breathingRate)) rpm is \(RRConstants.getClassification(value: breathingRate)) for healthy adults.")
+                        .font(.sprenLabel)
                     Spacer()
                 }
                 
@@ -37,10 +42,30 @@ struct DemographicRRCard: View {
     }
 }
 
-struct RespiratoryConstants {
-     static let rangeMin: CGFloat = 12
-     static let rangeMax: CGFloat = 20
- }
+struct RRConstants {
+    static let rangeMin: CGFloat = 12
+    static let rangeMax: CGFloat = 20
+    
+    static func getClassification(value: Double) -> String {
+        if value < 12 {
+            return "abnormally low"
+        }
+        if 12 <= value && value <= 20 {
+            return "normal"
+        }
+        return "abnormally high"
+    }
+    
+    static func getColor(value: Double) -> Color {
+        if value < 12 {
+            return Color("Yellow", bundle: .module)
+        }
+        if 12 <= value && value <= 20 {
+            return .green
+        }
+        return Color("Yellow", bundle: .module)
+    }
+}
 
 struct RespiratoryRateBarCard_Previews: PreviewProvider {
     static var previews: some View {
